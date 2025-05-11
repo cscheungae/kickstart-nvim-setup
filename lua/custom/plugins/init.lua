0ce0952ba14dbca1d3a4eb35717e692ddc512a83
billy <ccs99817@gmail.com>
2025-05-11 16:59:34 -0400

    Add auto-session to remember opened tabs

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -7,4 +7,14 @@ return {
     'karb94/neoscroll.nvim',
     opts = {},
   },
+  {
+    'rmagatti/auto-session',
+    config = function()
+      require('auto-session').setup {
+        auto_restore_enabled = true,
+        auto_save_enabled = true,
+        suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+      }
+    end,
+  },
 }
